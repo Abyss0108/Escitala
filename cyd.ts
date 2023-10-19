c0cae039d9c6cbb42e8f998c9c3eaa5d18f3f12e
Abyss0108 <147667703+Abyss0108@users.noreply.github.com>
2023-10-18 21:49:37 -0600

    Update and rename descodificar.vue to cyd.ts

---
+++ cyd.ts
@@ -0,0 +1,37 @@
+function escitalaEncrypt(message: string, turns: number): string {
+  const columnCount = turns;
+  const rowCount = Math.ceil(message.length / columnCount);
+  const matrix: string[][] = new Array(rowCount).fill('').map(_ => new Array(columnCount).fill(''));
+  let k = 0;
+
+  for (let i = 0; i < rowCount; i++) {
+    for (let j = 0; j < columnCount; j++) {
+      if (k < message.length) {
+        matrix[i][j] = message[k];
+        k++;
+      }
+    }
+  }
+
+  return matrix.flat().join('');
+}
+
+function escitalaDecrypt(encryptedMessage: string, turns: number): string {
+  const columnCount = turns;
+  const rowCount = Math.ceil(encryptedMessage.length / columnCount);
+  const matrix: string[][] = new Array(rowCount).fill('').map(_ => new Array(columnCount).fill(''));
+  let k = 0;
+
+  for (let j = 0; j < columnCount; j++) {
+    for (let i = 0; i < rowCount; i++) {
+      if (k < encryptedMessage.length) {
+        matrix[i][j] = encryptedMessage[k];
+        k++;
+      }
+    }
+  }
+
+  return matrix.flat().join('');
+}
+
+// Luego, en tus componentes Vue, puedes usar estas funciones en las funciones encode y decode
